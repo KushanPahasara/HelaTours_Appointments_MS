f0458092120f8704289ed29d850885ac8d4855e7
kushan <kushanpahasaradharmawansha@gmail.com>
2024-01-08 13:11:11 +0530

    User Save

--- src/main/java/com/example/appoinmentmsnew/controller/UserController.java
+++ src/main/java/com/example/appoinmentmsnew/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.example.appoinmentmsnew.controller;
 
+import com.example.appoinmentmsnew.dto.ResponseDTO;
+import com.example.appoinmentmsnew.dto.UserDTO;
+import com.example.appoinmentmsnew.service.UserService;
+import com.example.appoinmentmsnew.util.VarList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    public ResponseDTO responseDTO;
+
+    @PostMapping(value = "/saveUser")
+    public ResponseEntity saveUser(@RequestBody UserDTO userDTO) {
+        try{
+            String res= userService.saveUser(userDTO);
+            if(res.equals("00")){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(userDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }else if(res.equals("06")){
+                responseDTO.setCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMessage("Employee Registered");
+                responseDTO.setContent(userDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }else {
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+
+        }catch (Exception ex){
+            responseDTO.setCode(VarList.RSP_FAIL);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- src/main/java/com/example/appoinmentmsnew/service/UserService.java
+++ src/main/java/com/example/appoinmentmsnew/service/UserService.java
@@ -1,9 +1,31 @@
 package com.example.appoinmentmsnew.service;
 
 import com.example.appoinmentmsnew.dto.UserDTO;
+import com.example.appoinmentmsnew.entity.User;
+import com.example.appoinmentmsnew.repo.UserRepo;
+import com.example.appoinmentmsnew.util.VarList;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@Transactional
 public class UserService {
 
+    @Autowired
+    private UserRepo userRepo;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public String saveUser(UserDTO userDTO){
+        if(userRepo.existsById(userDTO.getUserId())){
+            return VarList.RSP_DUPLICATED;
+        }else{
+          userRepo.save(modelMapper.map(userDTO, User.class));
+          return VarList.RSP_SUCCESS;
+        }
     }
 }
